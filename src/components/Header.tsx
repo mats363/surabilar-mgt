f724a0e70552130b00534b450122f73d09fa475e
mats363 <81287846+mats363@users.noreply.github.com>
2022-10-21 13:57:33 +0200

    working

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import { Navbar } from "./Navbar";
 import logo from "../images/MGT-logo-scaled.jpg"
 import { Button } from "./Buttons";
 import hero1 from "../images/pngwing.com.png"
+import hero1svg from "../images/Chandra2.svg"
 
 
 
@@ -13,19 +14,39 @@ export function Header() {
                 <div className="h-77px bg-black">
                     <img src={logo}></img>
                 </div>
-                <div><Navbar/></div>
+                <nav className="flex justify-between">
+                <ul className="flex justify-between text-wind">
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/">Events</a>
+                    </li>
+                    <li>
+                        <a href="/">Local stores</a>
+                    </li>
+                    <li>
+                        <a href="/">Contact</a>
+                    </li>
+                </ul>
+        </nav>
+
             </div>
         </header>  
+
         <div className="flex justify-evenly">
             <div className="flex justify-center items-center flex-col space-y-4">
                 <Button textColor={"wind"} buttonColor={"fire"} buttonText={"Get started"}></Button>
                 <Button textColor={"fire"} buttonColor={"sun"} buttonText={"Gathering"}></Button> 
             </div>   
-        
-        
-            <div className="hidden tablet:block desktop:block">
-                <img src={hero1} className="h-72 bottom-20 scale-150 absolute"></img>
+            
+
+            <div className="hidden tablet:block desktop:block m-0 p-0">
+                {/* <img src={hero1} className="h-72 bottom-20 scale-150 absolute"></img> */}
+                <img src={hero1svg} className="absolute top-[53px] right-[0px] scale-90 p-0 m-0"></img>
+                {/* <img src={hero1svg}></img> */}
             </div>
+            
         </div>
         <div className="w-screen h-16 bg-black"></div>     
         
